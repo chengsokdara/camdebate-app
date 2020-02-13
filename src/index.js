e95e35ee405bca71465f07aca7f5406077cda003
Cheng Sokdara <chengsokdara@gmail.com>
2020-02-13 14:05:56 +0700

    Add OneSignal library.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
-import React from 'react'
-import Navigator from './screens'
-
+import React, { useEffect } from 'react'
+import OneSignal from 'react-native-onesignal'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { Provider as PaperProvider } from 'react-native-paper'
 import { Provider as ReduxProvider } from 'react-redux'
+
+import Navigator from './screens'
 import client from './service/client'
 import store from './service/store'
+import { ONESIGNAL_APP_ID } from './resources'
 import theme from './resources/theme'
 
 import { Layout } from './components'
 
 const App = () => {
+  const onReceived = notification => {
+    console.log('onReceived', notification)
+  }
+
+  const onOpened = openResult => {
+    console.log('onOpened', openResult)
+  }
+
+  const onIds = device => {
+    console.log('Device info: ', device)
+  }
+
+  useEffect(() => {
+    OneSignal.init(ONESIGNAL_APP_ID)
+    OneSignal.addEventListener('received', onReceived)
+    OneSignal.addEventListener('opened', onOpened)
+    OneSignal.addEventListener('ids', onIds)
+    return () => {
+      OneSignal.removeEventListener('received', onReceived)
+      OneSignal.removeEventListener('opened', onOpened)
+      OneSignal.removeEventListener('ids', onIds)
+    }
+  }, [])
   return (
     <ApolloProvider client={client}>
       <ReduxProvider store={store}>
--- src/resources/configs.js
+++ src/resources/configs.js
@@ -7,3 +7,5 @@ export const AppDrawerRouteName = 'Feed'
 export const GRAPHQL_LOCAL_TEST_SERVER = 'http://192.168.100.123:4000/'
 export const GRAPHQL_PRODUCTION_SERVER =
   'https://camdebate-graphql-server.herokuapp.com'
+
+export const ONESIGNAL_APP_ID = '4a322587-7897-41ca-a7b0-bc07d0cac3b2'
